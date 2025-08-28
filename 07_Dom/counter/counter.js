9b1746ab977a05a304db1873656f0a115c1cd3c4
deru0u0ww <mao20.cfs@icloud.com>
2025-08-29 00:01:46 +0900

    カウントアプリ完成

--- 07_Dom/counter/counter.js
+++ 07_Dom/counter/counter.js
@@ -5,6 +5,15 @@ const counter = {
     value: 0, //初期値
     elements: {}, //要素を格納するためのオブジェクト
     init() {
+
+        counter.createElements();
+        counter.bindEvents();
+        counter.update();
+        //初期表示
+        this.update();
+    },
+    createElements(){
+        //親要素の作成
         const container = document.createElement('div');
         document.body.appendChild(container);
         //追加ボタン要素
@@ -25,8 +34,10 @@ const counter = {
         container.appendChild(valueElem);
         
         //分割代入でelemensに代入することで、uppdateメソッド内でも参照できる
-        this.elements = { container, addBtn, downBtn, valueElem};
-
+        this.elements = { container, addBtn, downBtn, resetBtn,valueElem};
+    },
+    bindEvents() {
+        const { addBtn, downBtn, resetBtn } = this.elements;
         //追加ボタンのクリックイベント
         addBtn.addEventListener('click', ()=> {
             this.value++;
@@ -42,8 +53,6 @@ const counter = {
             this.value = 0;
             this.update();
         })
-        //初期表示
-        this.update();
     },
     update() {
         const { valueElem } = this.elements;
@@ -51,10 +60,6 @@ const counter = {
         valueElem.textContent = this.value;
         valueElem.style.color = this.value >= 0 ? 'green' : 'red';
     }
-
-
-    
-    
 }
 
 counter.init();
