9e68af122f518afd727691d791985fec3538d5de
deru0u0ww <mao20.cfs@icloud.com>
2025-08-28 10:55:57 +0900

    追加　データ型とテンプレートリテラルの学習

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,57 @@
+//typeof 値のデータ型を返す
+
+
+console.log(typeof "こんにちは"); //string
+console.log(typeof 123); //number
+console.log(typeof true); //boolean
+console.log(typeof false); //boolean
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof {}); //object
+console.log(typeof []); //object
+console.log(typeof function(){}); //function
+
+console.log(typeof ''); //string
+console.log(typeof 0); //number
+console.log(typeof NaN); //number
+
+//string
+//エスケープシーケンス
+let msg = 'こんにちは \n今日もいい天気ですね';
+console.log(msg);
+
+let data = ['Javascript','Java','Python'];
+console.log(data[0]); //Javascript
+
+//配列要素の追加
+data[3] = 'Ruby';
+console.log(data); //['Javascript', 'Java', 'Python', 'Ruby']
+
+//配列の要素の変更
+data[0] = 'C++';
+console.log(data); //['C++', 'Java', 'Python', 'Ruby']
+
+//オブジェクト
+let obj = {
+    name: '山田太郎',
+    age: 32,
+    birthday: '1998/050/19'
+}
+console.log(obj.name); //山田太郎
+console.log(obj['age']); //32
+console.log(typeof obj['age']); //number
+
+//undefinedとnull
+
+let x;
+console.log(x); //undefined
+//宣言済みだけど値が設定されていない場合
+//未定義の変数を参照しようとした
+//関数で値が返されない
+
+let y = null;
+console.log(y); //null
+
+let z = 123;
+z = null; //値が存在しないことを明示的に示す場合に使用
+console.log(z); //null
