c0e81af81c50898e75e0d9aeafef5539333f7e39
deru0u0ww <mao20.cfs@icloud.com>
2025-08-28 23:00:37 +0900

    増減ボタンの追加

--- 07_Dom/counter/counter.js
+++ 07_Dom/counter/counter.js
@@ -2,26 +2,45 @@
 //カウンターアプリ
 
 const counter = {
-    value: 0,
-    elements: {},
+    value: 0, //初期値
+    elements: {}, //要素を格納するためのオブジェクト
     init() {
         const container = document.createElement('div');
         document.body.appendChild(container);
-
+        //追加ボタン要素
         const addBtn = document.createElement('button');
         addBtn.textContent = '+';
         container.appendChild(addBtn);
-
+        //現象ボタン要素
         const downBtn = document.createElement('button');
         downBtn.textContent = '-';
         container.appendChild(downBtn);
-
-        const result = document.createElement('p');
-        result.textContent = this.value;
-        container.appendChild(result);
-
         
+        //値の表示
+        const valueElem = document.createElement('p');
+        container.appendChild(valueElem);
+        
+        this.elements = { container, addBtn, downBtn, valueElem};
+        //追加ボタンのクリックイベント
+        addBtn.addEventListener('click', ()=> {
+            this.value++;
+            this.update();
+            
+        });
+        //現象ボタンのクリックイベント
+        downBtn.addEventListener('click', ()=> {
+            this.value--;
+            this.update();
+        })
+        this.update();
+    },
+    update() {
+        this.elements.valueElem.textContent = this.value;
     }
+
+
+    
+    
 }
 
 counter.init();
