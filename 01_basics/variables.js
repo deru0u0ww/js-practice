75c0294c107f298517b8bbbdf6f04f805810a65d
deru0u0ww <mao20.cfs@icloud.com>
2025-08-28 10:28:53 +0900

    追加 変数/定数の挙動確認

---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//変数の宣言と代入
+let msg = 'こんにちは';
+console.log(msg);
+
+msg = 'こんばんは';
+console.log(msg);
+
+//変数の宣言のみ→undefined
+let msg1;
+console.log(msg1);
+
+//定数の宣言と代入
+const TAX = 1.1;
+const BIRTHDAY = '1998/05/19';
+console.log(100 * TAX);
+console.log(BIRTHDAY);
