4bf89ab357838baec27e522234de44749bf611b7
deru0u0ww <mao20.cfs@icloud.com>
2025-08-29 07:01:59 +0900

    変数の仕組みの復習

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -18,4 +18,33 @@ const BIRTHDAY = '1998/05/19';
 console.log(100 * TAX);
 console.log(BIRTHDAY);
 
-TAX = 1.2; //エラー
+// TAX = 1.2; //エラー
+
+//--------------------------------
+const playerName = '山田太郎';
+const age = 32;
+console.log('私の名前は' +playerName+ 'です。年齢は' + age + '歳です');
+
+//--------------------------------
+const a = 1;
+const b = 23;
+
+console.log('a + b = ' + (a + b)); //24
+console.log('a - b = ' + (a - b)); //-22
+console.log('a * b = ' + (a * b)); //23
+console.log('a / b = ' + (a / b)); //0.043478260869565216
+console.log('a % b = ' + (a % b)); //1
+
+//--------------------------------
+const m = 1;
+const n = 2;
+console.log('入れ替え前は：m = ' + m + ', n = ' + n); //m = 1, n = 2
+
+const tmp = m;
+console.log( tmp ); //1
+//入れ替え
+
+ m = n;
+ console.log( m ); //2 ;
+// n = tmp;
+// console.log('入れ替え後は：m = ' + m + ', n = ' + n); //m = 2, n = 1
